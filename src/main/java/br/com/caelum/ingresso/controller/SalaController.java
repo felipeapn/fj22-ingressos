d33351d72be50d7bce96cbcf3550288e0c6cb0c6
felipeapn <felipeapn@yahoo.com.br>
2018-04-12 14:51:15 -0300

    Exercicios 2.4 Adicionando Sessão ao Sistem

--- src/main/java/br/com/caelum/ingresso/controller/SalaController.java
+++ src/main/java/br/com/caelum/ingresso/controller/SalaController.java
@@ -1,6 +1,7 @@
 package br.com.caelum.ingresso.controller;
 
 import br.com.caelum.ingresso.dao.SalaDao;
+import br.com.caelum.ingresso.dao.SessaoDao;
 import br.com.caelum.ingresso.model.Sala;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -21,6 +22,8 @@ public class SalaController {
     @Autowired
     private SalaDao salaDao;
 
+    @Autowired
+    private SessaoDao sessaoDao;
 
     @GetMapping({"/admin/sala", "/admin/sala/{id}"})
     public ModelAndView form(@PathVariable("id")Optional<Integer> id, Sala sala){
@@ -78,7 +81,8 @@ public class SalaController {
 
         Sala sala = salaDao.findOne(id);
         modelAndView.addObject("sala", sala);
-
+        modelAndView.addObject("sessoes", sessaoDao.buscaSessoesDaSala(sala));
+        
         return modelAndView;
     }
 
